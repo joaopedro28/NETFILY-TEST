96897a99ec5caf9f7f8a14919b0ea73a653c5a19
Joao Pedro Rocha <joaopedro@wdhouse.com.br>
2021-02-24 00:27:21 -0300

    fisrt commit

---
+++ nuxt.config.js
@@ -0,0 +1,85 @@
+export default {
+	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+	ssr: false,
+
+	// Target: https://go.nuxtjs.dev/config-target
+	target: 'static',
+
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'snipcart-nuxt-pwa',
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{ hid: 'description', name: 'description', content: '' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/bootstrap
+		'bootstrap-vue/nuxt',
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+		// https://go.nuxtjs.dev/pwa
+		'@nuxtjs/pwa',
+	],
+
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {},
+
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		manifest: {
+      name: 'Nuxt.js PWA DOS GURIIIIIII',
+      short_name: 'MINHA-PWA',
+      lang: 'en',
+      display: 'standalone',		},
+	},
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+  workbox: {
+    runtimeCaching: [
+      {
+        urlPattern: 'https://fonts.googleapis.com/.*',
+        handler: 'cacheFirst',
+        method: 'GET',
+        strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
+      },
+      {
+        urlPattern: 'https://fonts.gstatic.com/.*',
+        handler: 'cacheFirst',
+        method: 'GET',
+        strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
+      },
+      {
+        urlPattern: 'https://cdn.snipcart.com/.*',
+        method: 'GET',
+        strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
+      },
+      {
+        urlPattern: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
+        handler: 'cacheFirst',
+        method: 'GET',
+        strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
+      }
+    ]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="container">
+		<div>
+			<div>
+				<b-card
+					title="Card Title"
+					img-src="https://picsum.photos/600/300/?image=25"
+					img-alt="Image"
+					img-top
+					tag="article"
+					style="max-width: 20rem"
+					class="mb-2"
+				>
+					<b-card-text>
+						Some quick example text to build on the card
+						title and make up the bulk of the card's
+						content.
+					</b-card-text>
+
+					<b-button href="#" variant="primary"
+						>Go somewhere</b-button
+					>
+				</b-card>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+	margin: 0 auto;
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
+
+.title {
+	font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+		BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+		sans-serif;
+	display: block;
+	font-weight: 300;
+	font-size: 100px;
+	color: #35495e;
+	letter-spacing: 1px;
+}
+
+.subtitle {
+	font-weight: 300;
+	font-size: 42px;
+	color: #526488;
+	word-spacing: 5px;
+	padding-bottom: 15px;
+}
+
+.links {
+	padding-top: 15px;
+}
+</style>
